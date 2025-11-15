06b142ae275904b46ca48f57d0706d2e56f764bc
lakma <lakmalaberathne@gmail.com>
2025-11-15 11:54:44 +0530

    feat: use Local ollama model gemma3:1B for general chat

--- app/agents/llm_provider.py
+++ app/agents/llm_provider.py
@@ -1,3 +1,4 @@
+from langchain_community.chat_models import ChatOllama
 from langchain_openai import ChatOpenAI
 from app.core.config import settings
 from app.enums import AiModel
@@ -5,9 +6,16 @@ from app.enums import AiModel
 def get_llm(temperature: float = 0, model: AiModel = AiModel.GPT_5_NANO):
         if not settings.OPENAI_API_KEY:
             raise ValueError("OpenAI API key not configured")
-        
-        return ChatOpenAI(
-            model= model,
-            temperature=temperature,
-            openai_api_key=settings.OPENAI_API_KEY
-        )
+
+        if model == AiModel.GPT_5_NANO:
+            return ChatOpenAI(
+                model= model,
+                temperature=temperature,
+                openai_api_key=settings.OPENAI_API_KEY
+            )
+
+        elif model == AiModel.GEMMA3:
+            return ChatOllama (
+                model=model,
+                temperature=temperature
+            )
--- app/enums/ai_model.py
+++ app/enums/ai_model.py
@@ -3,3 +3,4 @@ from enum import Enum
 class AiModel(str, Enum):
     GPT_5_NANO = "gpt-5-nano"
     LLAMA3_2 = "llama3.2"
+    GEMMA3 = "gemma3:1b"
